521854b3f7143947fea0aadec48dda0dcd447822
kam515 <112594350+kam515@users.noreply.github.com>
2024-11-02 21:10:29 -0500

    small update

--- app_folder/app.py
+++ app_folder/app.py
@@ -7,3 +7,5 @@ add_auth(required=True)
 #after authentication, the email and subscription status is stored in session state
 st.write(st.session_state.email)
 st.write(st.session_state.user_subscribed)
+
+st.write('hi')
