2ff40dc048da7cd021f9dc203fdfa3239e3df530
kam515 <112594350+kam515@users.noreply.github.com>
2024-11-10 21:37:44 -0600

    adding a success message

--- app_folder/app.py
+++ app_folder/app.py
@@ -2,10 +2,24 @@ import streamlit as st
 
 from st_paywall import add_auth
 
-add_auth(required=True)
+# add_auth(required=True)
 
-#after authentication, the email and subscription status is stored in session state
-st.write(st.session_state.email)
-st.write(st.session_state.user_subscribed)
+# #after authentication, the email and subscription status is stored in session state
+# st.write(st.session_state.email)
+# st.write(st.session_state.user_subscribed)
 
-st.write('hi')
+# st.markdown('hi')
+
+st.set_page_config(layout="wide")
+st.title("🎈 Katie's Subscription app POC 🎈")
+st.balloons()
+
+add_auth(
+    required=True,
+    login_button_text="Login with Google",
+    login_button_color="#FD504D",
+    login_sidebar=True,
+)
+
+st.write("Congrats, you are subscribed!")
+st.write("the email of the user is " + str(st.session_state.email))
