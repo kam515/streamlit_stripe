109452249c4854434343565bc7fa78e39d4b7c83
kam515 <112594350+kam515@users.noreply.github.com>
2024-11-16 17:52:58 -0600

    manually adding authentications

--- app_folder/app.py
+++ app_folder/app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from st_paywall import add_auth
 
+add_auth(required=True)
+
+#after authentication, the email and subscription status is stored in session state
+st.write(st.session_state.email)
+st.write(st.session_state.user_subscribed)
+
 st.markdown('hi')
 st.markdown('hi again')
 
